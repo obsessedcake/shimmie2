8e55cb31de96fde63711f652113a4f482dac3e8c
Obsessed Cake <obsessed-cake@proton.me>
2024-05-14 01:35:25 +0300

    [upload] simplify danbooru transloader

--- ext/upload/danbooru.php
+++ ext/upload/danbooru.php
@@ -6,9 +6,6 @@ namespace Shimmie2;
 
 class DanbooruTransloader
 {
-    private const TAG_CATEGORY_PREFIX = 'tag_string_';
-    private const TAG_SERIES_PREFIX = '_(series)';
-
     public static function accept(string $url): bool
     {
         return str_contains($url, 'https://danbooru.donmai.us/');
@@ -17,55 +14,56 @@ class DanbooruTransloader
     public static function enrich_metadata(string $url, int $slot, array &$metadata): void
     {
         $json_url = self::clean_url($url).'.json';
-        $json = self::fetch_json($json_url);
+        $raw_json = self::fetch($json_url);
 
-        $final_tags = [];
-        foreach ($json as $key => $value) {
-            if (!str_starts_with($key, self::TAG_CATEGORY_PREFIX)) {
-                continue;
-            }
-
+        $tags = [];
+        foreach (json_decode($raw_json, true) as $key => $value) {
             if (empty($value)) {
                 continue;
             }
 
-            $tag_category = substr($key, strlen(self::TAG_CATEGORY_PREFIX)) . ':';
-            if ($tag_category === 'general:') {
-                $final_tags[] = $value;
+            $tag_category = str_replace('tag_string_', '', $key, $is_category);
+            if (!$is_category) {
                 continue;
             }
 
-            $tags = explode(' ', $value);
-
-            if ($tag_category === 'copyright:') {
-                foreach ($tags as $tag) {
-                    if (str_ends_with($tag, self::TAG_SERIES_PREFIX)) {
-                        $fixed_tag = 'series:'.substr($tag, 0, -strlen(self::TAG_SERIES_PREFIX));
-                    } else {
-                        $fixed_tag = $tag_category.$tag;
-                    }
-
-                    $final_tags[] = $fixed_tag;
-                }
-                continue;
+            if ($tag_category == 'general') {
+                $tag_category = NULL;
             }
 
-            $final_tags = array_merge($final_tags, substr_replace($tags, $tag_category, 0, 0));
+            foreach (explode(' ', $value) as $tag) {
+                $fixed_tag = preg_replace(
+                    pattern: '/^(.*)_\((place|series)\)$/',
+                    replacement: '\2:\1',
+                    subject: $tag,
+                    limit: 1,
+                    count: $is_replaced
+                );
+
+                if (!$is_replaced && $tag_category) {
+                    $fixed_tag = "$tag_category:$tag";
+                } 
+
+                $tags[] = $fixed_tag;
+            }
         }
 
         $metadata['rating'.$slot] = self::convert_rating($json['rating']);
         $metadata['source'.$slot] = self::clean_url($json['source']);
-        $metadata['tags'.$slot] = implode(' ', $final_tags);
+        $metadata['tags'.$slot] = implode(' ', $tags);
         $metadata['url'.$slot] = $json['file_url'];
     }
 
+    /**
+     * Clean URL from fragment and query options.
+     */
     private static function clean_url(string $url): string
     {
         $url_parts = parse_url($url);
         return $url_parts['scheme'] . '://' . $url_parts['host'] . $url_parts['path'];
     }
 
-    private static function fetch_json(string $url): array
+    private static function fetch(string $url): array
     {
         $ch = curl_init($url);
         assert($ch !== false);
@@ -83,7 +81,7 @@ class DanbooruTransloader
         }
 
         curl_close($ch);
-        return json_decode($response, true);
+        return $response;
     }
 
     private static function convert_rating(string $rating): string
